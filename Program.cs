a6ccf944ab17c877582d346f697832d4a119200e
mdanello <mdanello26@gmail.com>
2024-03-21 18:44:47 -0400

    Setup test menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+while (true)
+{
+    Console.WriteLine("Select Test:");
+    Console.WriteLine("   (1) - Fibonacci Sequence");
+    Console.WriteLine("   (2) - Find the first solo");
+    Console.WriteLine("   (X) - Fibonacci Sequence");
+    var response = Console.ReadKey().KeyChar;
+    if (Char.ToLower(response) == 'x')
+    {
+        Console.WriteLine($"\nProgram Exit!\n");
+        break;
+    }
+    Console.WriteLine($"\n\n");
+}
